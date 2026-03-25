4cea088ccf6f6fbdfdc232a6689efd2ff1b1502b
hmad213 <142343785+hmad213@users.noreply.github.com>
2026-03-25 17:10:50 +0500

    Backend Setup

---
+++ server/src/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+app.listen(PORT, (error) => {
+  if (error) {
+    throw error;
+  }
+
+  console.log("Listening on Port " + 3000 + "!");
+});
