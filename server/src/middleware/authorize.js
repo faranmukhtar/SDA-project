0db35d4caf8e518117cf7e4337034606c4d39f68
hmad213 <142343785+hmad213@users.noreply.github.com>
2026-04-25 10:18:13 +0500

    Add authorize ID middleware

--- server/src/middleware/authorize.js
+++ server/src/middleware/authorize.js
@@ -1,4 +1,4 @@
-const authorize =
+const authorizeRole =
   (...roles) =>
   (req, res, next) => {
     if (!roles.includes(req.user?.role)) {
@@ -7,6 +7,27 @@ const authorize =
     next();
   };
 
+const authorizeId =
+  (...bypassRoles) =>
+  (req, res, next) => {
+    if (bypassRoles.length !== 0 && bypassRoles.includes(req.user?.role)) {
+      return next(); // privileged role, skip ID check
+    }
+
+    const index = parseInt(req.params.index, 10);
+
+    if (isNaN(index)) {
+      return res.status(400).json({ error: "Invalid ID" });
+    }
+
+    if (index !== req.user?.id) {
+      return res.status(403).json({ error: "Forbidden" });
+    }
+
+    next();
+  };
+
 module.exports = {
-  authorize,
+  authorizeRole,
+  authorizeId,
 };
